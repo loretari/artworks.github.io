8dbbd4f1c6cc6eb4569ea6689abed27493c6b882
loretari <loretari@gmail.com>
2023-03-01 11:02:48 +0200

    init: create pagination function in model.js

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -12,9 +12,10 @@ class ResultsView extends View{
         return this._data.map(this._generateMarkupPreview).join('');
     }
     _generateMarkupPreview (result) {
+        console.log(result);
         return `
         <li class="preview">
-              <a class="preview__link" href="#${result.id}">
+              <a class="preview__link preview__link--active" href="#${result.id}">
                 <div class="preview__data">
                   <h4 class="preview__title">${result.title}</h4>
                   <p class="preview__publisher">${result.model}</p>
