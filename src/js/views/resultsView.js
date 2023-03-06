5745a4ce4cbeab8b1c8dc383fa07389caa438b1f
loretari <loretari@gmail.com>
2023-03-06 20:30:53 +0200

    init: fix  bug in bookmarksView.js

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -18,18 +18,16 @@ class ResultsView extends View{
         <li class="preview">
               <a class="preview__link ${result.id === id ? "preview__link--active" : ''}" href="#${result.id}">
                 <div class="preview__data">
-                 <p class="preview__publisher "> Was performed in: </p>
-<h4 class="preview__title">${result.date_display}</h4>
- </div>
-                  <div class="preview__data">
+                
+          
                   <h4 class="preview__title">${result.title}</h4>
-                  <p class="preview__publisher">${result.artist_title}</p>
+                  <p class="preview__publisher">${result.model}</p>
                   <div class="preview__user-generated">
                     <svg>
                       <use href="${icons}#icon-user"></use>
                     </svg>
                   </div>
-               
+               </div>
               </a>
             </li>`;
     }
