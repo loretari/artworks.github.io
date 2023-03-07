f0a5705ca54eccd14acb8d9956858e48426cc6d0
loretari <loretari@gmail.com>
2023-03-07 12:14:27 +0200

    init: fix bug

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -13,31 +13,31 @@ class BookmarksView extends View{
         console.log(this._data);
         return this._data.map(bookmark => previewView.render(bookmark, false)).join('');
     }
-//     _generateMarkupPreview (result) {
-//         const id = window.location.hash.slice(1);
-//         console.log(result);
-//         return `
-//         <li class="preview">
-//                               <a class="preview__link" href="#${result.id}">
-//                                 <div class="preview__data">
-//
-//                               <p class="preview__publisher "> Was performed in: </p>
-// <h4 class="preview__title">${result.date_display}</h4>
-//
-//                                 </div>
-//                                 <div class="preview__data">
-//                                   <h4 class="preview__title">
-//                                     ${result.title}
-//                                   </h4>
-//                                   <p class="preview__publisher">${result.artist_title}</p>
-//                                     <div class="preview__user-generated">
-//                     <svg>
-//                       <use href="${icons}#icon-user"></use>
-//                     </svg>
-//                   </div>
-//                                 </div>
-//                               </a>
-//                             </li>`;
-//     }
+    _generateMarkupPreview (result) {
+        const id = window.location.hash.slice(1);
+        console.log(result);
+        return `
+        <li class="preview">
+                              <a class="preview__link" href="#${result.id}">
+                                <div class="preview__data">
+
+                              <p class="preview__publisher "> Was performed in: </p>
+<h4 class="preview__title">${result.date_display}</h4>
+
+                                </div>
+                                <div class="preview__data">
+                                  <h4 class="preview__title">
+                                    ${result.title}
+                                  </h4>
+                                  <p class="preview__publisher">${result.artist_title}</p>
+                                    <div class="preview__user-generated">
+                    <svg>
+                      <use href="${icons}#icon-user"></use>
+                    </svg>
+                  </div>
+                                </div>
+                              </a>
+                            </li>`;
+    }
 }
 export default new BookmarksView();
