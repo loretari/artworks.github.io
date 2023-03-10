658ba9745d77a9daa58b7578494da11c66cfe741
loretari <loretari@gmail.com>
2023-03-10 10:28:34 +0200

    init: fix bug in package.json

--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,5 @@
 import {async} from "regenerator-runtime";
-import 'regenerator-runtime';
+import 'regenerator-runtime/runtime';
 
 import {API_URL, RES_PER_PAGE, KEY} from "./config";
 import {AJAX} from "./helpers";
